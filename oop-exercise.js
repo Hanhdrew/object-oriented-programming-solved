d7d4c781647e8b000e90003272a0ceee28bda328
Hanhdrew <andrew.rebbe@gmail.com>
2026-04-09 22:23:20 -0700

    added a few class methods

--- oop-exercise.js
+++ oop-exercise.js
@@ -1,1 +1,49 @@
 /* Please read the instructions located in the exercises/2-oop-exercise-readme.md file and complete your code below */
+
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.grades = [];
+  }
+  addGrade(grade) {
+    if (grade >= 0 && grade <= 100) {
+      this.grades.push(grade);
+    } else {
+      console.log("Grade must be between 0 and 100.");
+    }
+  }
+  calculateAverage() {
+    if (this.grades.length === 0) {
+      return 0;
+    } else {
+      let sum = this.grades.reduce((acc, curr) => {
+        return (acc = acc + curr);
+      }, 0);
+      return sum / this.grades.length;
+    }
+  }
+  hasPassed(passingGrade) {
+    if (passingGrade <= this.calculateAverage()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+}
+
+class Course {}
+
+const newStudent = new Student("andrew", 380);
+
+newStudent.addGrade(40);
+newStudent.addGrade(60);
+newStudent.addGrade(70);
+
+console.log(newStudent.hasPassed(30));
